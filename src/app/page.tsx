8c515902e9f82ae6cbd93706086662dc890e2cb2
Mohamed <tonemail@gmail.com>
2026-06-13 13:30:20 +0200

    Ameliorer accueil HelpFlow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
 import Header from "../components/landing/Header";
 import Hero from "../components/landing/Hero";
-import SpaceChoice from "../components/landing/SpaceChoice";
 import WhyHelpFlow from "../components/landing/WhyHelpFlow";
 import Footer from "../components/landing/Footer";
 
 export default function Home() {
   return (
-    <main className="bg-white">
+    <main className="min-h-screen bg-slate-50">
       <Header />
       <Hero />
-      <SpaceChoice />
       <WhyHelpFlow />
       <Footer />
     </main>
-  )
+  );
 }
