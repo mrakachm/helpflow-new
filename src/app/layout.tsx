d0291fb93ec7913f49e7c1b246586056d32b44f8
Mohamed <tonemail@gmail.com>
2026-06-18 11:51:57 +0200

    Fix layout css import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
-import "./globals.css";
+import GoogleMapsScript from "@/components/GoogleMapsScript";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="fr">
       <body className="relative min-h-screen bg-white text-gray-900">
+        <GoogleMapsScript />
         {children}
       </body>
     </html>
